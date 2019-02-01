b4abfba2597f3bba0d344111e5439b0c312941fd
Jonathan Akira Tanita <jonathan.akira@bb.com.br>
2019-02-01 11:15:23 -0200

    alterações de hashmap para entidades de bb giro

--- src/centralcapturasjudicial/model/entity/bbgiro/AberturaTeto.java
+++ src/centralcapturasjudicial/model/entity/bbgiro/AberturaTeto.java
@@ -127,25 +127,30 @@ public class AberturaTeto implements Serializable, AbstractEntity {
         
         HashMap<Integer,String> values = new HashMap<>();
         
-        values.put(1, fd.format(getDtInicio()));
-        values.put(2, fd.format(getDtFim()));
-        values.put(3, String.format("%.2f", getVlTaxa()).replace(".", ","));
-//        values.put(1, Integer.toString(getMesCartao().getExtrato().getCartao().getOperacao()));
-//        values.put(2, getMesCartao().getDataVencimento().replace(".", "/"));
-//        if(Double.toString(getMesCartao().getTxJuros()) != null) {
-//            values.put(3, String.format("%.2f", (getMesCartao().getTxJuros())).replace(".", ","));
-//        }
-//        else {
-//            values.put(3, ""); 
-//        }
-//        values.put(4, getDataTransacao().replace(".", "/"));
-//        values.put(5, Integer.toString(getNrCartao()));
-//        values.put(6, getDescricao());
-//        values.put(7, String.format("%.2f", (getValor())).replace(".", ","));        
-//        values.put(8, getMoeda());
-        //values.put(9, String.format("%.2f", 1.5));
-        //values.put(6, getMes().getDataFaturamento());
-                        
+        Date dtInicio2 = getDtInicio();
+        Date dtFim2 = getDtFim();
+        Double vlTaxa2 = getVlTaxa();
+        
+        if(dtInicio2 != null){
+            values.put(1, fd.format(dtInicio2));
+        } 
+        else{
+            values.put(1, "");
+        }
+        
+        if(dtFim2 != null){
+            values.put(2, fd.format(dtFim2));
+        }
+        else{
+            values.put(2, "");
+        }
+        
+        if(vlTaxa2 != null){
+            values.put(3, String.format("%.3f", vlTaxa2).replace(".", ","));
+        }
+        else{
+            values.put(3, "");
+        }   
         return values;
     }
     
